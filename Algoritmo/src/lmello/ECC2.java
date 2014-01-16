9b863bad56c8de99e2a83f2cde56b4c425146180
lmello <lucashsmello@gmail.com>
2014-01-16 08:32:34 -0200

    Novo lmelloECC.

---
+++ Algoritmo/src/lmello/ECC2.java
@@ -0,0 +1,73 @@
+package lmello;
+
+import java.util.Arrays;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.trees.J48;
+import weka.core.Instance;
+import mulan.classifier.InvalidDataException;
+import mulan.classifier.MultiLabelOutput;
+import mulan.classifier.transformation.EnsembleOfClassifierChains;
+
+public class ECC2 extends EnsembleOfClassifierChains {
+
+	/**
+	 * Default constructor
+	 */
+	public ECC2() {
+		super(new J48(), 10, true, true);
+	}
+
+	public ECC2(Classifier c, int i, boolean b, boolean c2) {
+		super(c, i, b, c2);
+	}
+
+	public MultiLabelOutput makePredictionInternal(Instance instance)
+			throws Exception, InvalidDataException {
+
+		int[] sumVotes = new int[numLabels];
+		double[] sumConf = new double[numLabels];
+		MultiLabelOutput[] mloensemble = new MultiLabelOutput[numOfModels];
+
+		Arrays.fill(sumVotes, 0);
+		Arrays.fill(sumConf, 0);
+
+		for (int i = 0; i < numOfModels; i++) {
+			mloensemble[i] = ensemble[i].makePrediction(instance);
+		}
+
+		return combineMLO2(mloensemble);
+	}
+
+	private MultiLabelOutput combineMLO2(MultiLabelOutput[] mloensemble) {
+		double[] c = mloensemble[0].getConfidences();
+		boolean[] Fbipart = new boolean[mloensemble[1].getBipartition().length];
+		double[] Fconf = new double[mloensemble[1].getConfidences().length];
+		double maxprod = c[0];
+		Fconf[0] = c[0];
+		for (int i = 1; i < Fconf.length; i++) {
+			Fconf[i] = c[i];
+			maxprod *= c[i] >= 0.5 ? c[i] : 1 - c[i];
+		}
+
+		for (int i = 1; i < mloensemble.length; i++) {
+			double[] conf = mloensemble[i].getConfidences();
+			double prod = conf[0];
+
+			for (int j = 1; j < conf.length; j++) {
+				prod *= c[j] >= 0.5 ? c[j] : 1 - c[j];
+			}
+			if (prod > maxprod) {
+				maxprod = prod;
+				for (int j = 0; j < conf.length; j++) {
+					Fconf[j] = c[j];
+				}
+			}
+		}
+
+		for (int i = 0; i < Fconf.length; i++) {
+			Fbipart[i] = Fconf[i] > 0.5;
+		}
+		return new MultiLabelOutput(Fbipart, Fconf);
+	}
+}
